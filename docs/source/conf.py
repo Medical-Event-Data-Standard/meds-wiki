eaeb77bba7be887184e74e22f5dbe9aa581a33fa
Robin van de Water <r.p.vandewater@gmail.com>
2024-06-11 17:32:48 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MEDS'
+copyright = 'MEDS Working Group'
+author = 'MEDS Working Group'
 
 release = '0.1'
 version = '0.1.0'
